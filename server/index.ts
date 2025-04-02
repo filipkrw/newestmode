a0a91b56c2ede1009ad02a38840ea61ed91d27a8
Filip Krawczyk <filip@algomo.com>
2025-04-02 12:49:27 +0100

    wip

---
+++ server/index.ts
@@ -0,0 +1,31 @@
+import {
+  fastifyTRPCPlugin,
+  type FastifyTRPCPluginOptions,
+} from "@trpc/server/adapters/fastify";
+import fastify from "fastify";
+import { createContext } from "./context";
+import { appRouter, type AppRouter } from "./router";
+
+const server = fastify({
+  maxParamLength: 5000,
+});
+
+server.register(fastifyTRPCPlugin, {
+  prefix: "/trpc",
+  trpcOptions: {
+    router: appRouter,
+    createContext,
+    onError({ path, error }) {
+      // report to error monitoring
+      console.error(`Error in tRPC handler on path '${path}':`, error);
+    },
+  } satisfies FastifyTRPCPluginOptions<AppRouter>["trpcOptions"],
+});
+
+try {
+  await server.listen({ port: 3001 });
+  console.log(`Server is running at http://localhost:3001`);
+} catch (err) {
+  server.log.error(err);
+  process.exit(1);
+}
